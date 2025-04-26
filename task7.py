a1a7cd2d6cfde6498db233d49b3ad9f3d16e4c2d
Герман Селенских <selenskih.german@yandex.ru>
2025-04-26 10:01:27 +0300

    Update task7.py

--- task7.py
+++ task7.py
@@ -4,21 +4,31 @@ class BinaryTreeNode:
         self.left = None
         self.right = None
 
-def restore_tree(logs):
-    node_dict = {}
-    for log in logs:
-        value = log['value']
-        node_dict[value] = BinaryTreeNode(value)
-
-    for log in logs:
-        value = log['value']
-        if log['left'] is not None:
-            node_dict[value].left = node_dict[log['left']]
-        if log['right'] is not None:
-            node_dict[value].right = node_dict[log['right']]
-
-    return node_dict[logs[0]['value']]  # корень
-
-# Пример использования
-# logs = [{'value': 1, 'left': 2, 'right': 3}, ...]
-# root = restore_tree(logs)
+def restore_tree(path):
+    nodes = {}
+    children = set()
+    with open(path) as f:
+        for line in f:
+            # Пример строки: "INFO 12:00:00 Visited node 1: left=2, right=3"
+            m = re.search(r"node (\d+): left=(\d+|None), right=(\d+|None)", line)
+            if m:
+                v, l, r = m.groups()
+                v = int(v)
+                if v not in nodes:
+                    nodes[v] = BinaryTreeNode(v)
+                node = nodes[v]
+                if l != "None":
+                    l = int(l)
+                    if l not in nodes:
+                        nodes[l] = BinaryTreeNode(l)
+                    node.left = nodes[l]
+                    children.add(l)
+                if r != "None":
+                    r = int(r)
+                    if r not in nodes:
+                        nodes[r] = BinaryTreeNode(r)
+                    node.right = nodes[r]
+                    children.add(r)
+    # Root is the node that is not a child
+    root_val = (set(nodes.keys()) - children).pop()
+    return nodes[root_val]
