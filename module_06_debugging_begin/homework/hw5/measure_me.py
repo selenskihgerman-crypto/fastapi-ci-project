85ca2cc1683435b94950699710d99054f4c381b0
Герман Селенских <selenskih.german@yandex.ru>
2025-04-27 09:07:46 +0300

    Update measure_me.py

--- module_06_debugging_begin/homework/hw5/measure_me.py
+++ module_06_debugging_begin/homework/hw5/measure_me.py
@@ -6,61 +6,23 @@
 в начале и в конце которой пишется "Enter measure_me" и "Leave measure_me".
 Сконфигурируйте логгер, запустите программу, соберите логи и посчитайте среднее время выполнения функции measure_me.
 """
-import logging
-import random
-from typing import List
+import re
+from datetime import datetime
 
-logger = logging.getLogger(__name__)
+def parse_time(s):
+    # Ожидаемый формат: HH:MM:SS,mmm
+    return datetime.strptime(s, "%H:%M:%S,%f")
 
+with open("measure_me.log") as f:
+    lines = [line for line in f if "Enter measure_me" in line or "Leave measure_me" in line]
 
-def get_data_line(sz: int) -> List[int]:
-    try:
-        logger.debug("Enter get_data_line")
-        return [random.randint(-(2 ** 31), 2 ** 31 - 1) for _ in range(sz)]
-    finally:
-        logger.debug("Leave get_data_line")
+times = []
+for i in range(0, len(lines), 2):
+    enter = re.search(r"(\d{2}:\d{2}:\d{2},\d{3})", lines[i]).group(1)
+    leave = re.search(r"(\d{2}:\d{2}:\d{2},\d{3})", lines[i+1]).group(1)
+    t1, t2 = parse_time(enter), parse_time(leave)
+    times.append((t2 - t1).total_seconds())
 
+print("Average execution time:", sum(times)/len(times), "seconds")
 
-def measure_me(nums: List[int]) -> List[List[int]]:
-    logger.debug("Enter measure_me")
-    results = []
-    nums.sort()
-
-    for i in range(len(nums) - 2):
-        logger.debug(f"Iteration {i}")
-        left = i + 1
-        right = len(nums) - 1
-        target = 0 - nums[i]
-        if i == 0 or nums[i] != nums[i - 1]:
-            while left < right:
-                s = nums[left] + nums[right]
-                if s == target:
-                    logger.debug(f"Found {target}")
-                    results.append([nums[i], nums[left], nums[right]])
-                    logger.debug(
-                        f"Appended {[nums[i], nums[left], nums[right]]} to result"
-                    )
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-                elif s < target:
-                    logger.debug(f"Increment left (left, right) = {left, right}")
-                    left += 1
-                else:
-                    logger.debug(f"Decrement right (left, right) = {left, right}")
-
-                    right -= 1
-
-    logger.debug("Leave measure_me")
-
-    return results
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level="DEBUG")
-    for it in range(15):
-        data_line = get_data_line(10 ** 3)
-        measure_me(data_line)
+        
