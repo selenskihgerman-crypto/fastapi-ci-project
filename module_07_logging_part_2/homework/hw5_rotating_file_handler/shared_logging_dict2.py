2838b78ac0fedc21a22bd8af260ccfcd46f9715e
Герман Селенских <selenskih.german@yandex.ru>
2025-06-04 14:17:01 +0300

    Add new file

---
+++ module_07_logging_part_2/homework/hw5_rotating_file_handler/shared_logging_dict2.py
@@ -0,0 +1,22 @@
+# Задача 5. Ротация логов (TimedRotatingFileHandler для utils)
+
+'handlers': {
+    # ...
+    'utils_rotating': {
+        'class': 'logging.handlers.TimedRotatingFileHandler',
+        'formatter': 'full',
+        'filename': 'utils.log',
+        'when': 'h',
+        'interval': 1,
+        'backupCount': 10,
+        'level': 'INFO'
+    }
+},
+'loggers': {
+    'utils': {
+        'handlers': ['utils_rotating'],
+        'level': 'INFO',
+        'propagate': False
+    }
+}
+# Только для utils, только INFO и выше.
