03af3e1ccd121608f9ab0ed6b93cb446a2286c9e
Герман Селенских <selenskih.german@yandex.ru>
2025-06-04 14:13:23 +0300

    Update utils.py

--- module_07_logging_part_2/homework/hw1_add_logging/utils.py
+++ module_07_logging_part_2/homework/hw1_add_logging/utils.py
@@ -1,29 +1,12 @@
+# Дополнение к первой задаче
 
-from typing import Union, Callable
-from operator import sub, mul, truediv, add
+import logging
+logger = logging.getLogger("utils")
 
+def add(x, y):
+    logger.debug(f"add({x}, {y})")
+    return x + y
 
-OPERATORS = {
-    '+': add,
-    '-': sub,
-    '*': mul,
-    '/': truediv,
-}
-
-Numeric = Union[int, float]
-
-
-def string_to_operator(value: str) -> Callable[[Numeric, Numeric], Numeric]:
-    """
-    Convert string to arithmetic function
-    :param value: basic arithmetic function
-    """
-    if not isinstance(value, str):
-        print("wrong operator type", value)
-        raise ValueError("wrong operator type")
-
-    if value not in OPERATORS:
-        print("wrong operator value", value)
-        raise ValueError("wrong operator value")
-
-    return OPERATORS[value]
+def subtract(x, y):
+    logger.debug(f"subtract({x}, {y})")
+    return x - y
