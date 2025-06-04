5a523c04fb9208604541c1beedaba06b67d16643
Герман Селенских <selenskih.german@yandex.ru>
2025-06-04 14:19:40 +0300

    Update shared_logging_dict2.py

--- module_07_logging_part_2/homework/hw5_rotating_file_handler/shared_logging_dict2.py
+++ module_07_logging_part_2/homework/hw5_rotating_file_handler/shared_logging_dict2.py
@@ -1,22 +1,28 @@
 # Задача 5. Ротация логов.
 
-'handlers': {
-    # ...
-    'utils_rotating': {
-        'class': 'logging.handlers.TimedRotatingFileHandler',
-        'formatter': 'full',
-        'filename': 'utils.log',
-        'when': 'h',
-        'interval': 1,
-        'backupCount': 10,
-        'level': 'INFO'
-    }
-},
-'loggers': {
-    'utils': {
-        'handlers': ['utils_rotating'],
-        'level': 'INFO',
-        'propagate': False
-    }
-}
-# Только для utils, только INFO и выше.
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+# Создаем логгер для utils
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)  # Только INFO и выше
+
+# Создаем обработчик с ротацией по времени
+handler = TimedRotatingFileHandler(
+    'utils.log',  # Имя файла
+    when='H',     # Ротация по часам
+    interval=10,  # Каждые 10 часов
+    backupCount=1, # Храним только 1 резервную копию (текущий + предыдущий)
+)
+
+# Настраиваем формат сообщений
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# Добавляем обработчик к логгеру
+logger.addHandler(handler)
+
+# Пример использования
+logger.info('Это информационное сообщение')
+logger.warning('Это предупреждение')
+logger.debug('Это сообщение отладки - не должно попасть в лог')  # Не попадет в лог
