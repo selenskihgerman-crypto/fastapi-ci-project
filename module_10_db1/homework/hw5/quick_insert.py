db7a760798483e4d20cd470e12ecd9185c293516
Герман Селенских <selenskih.german@yandex.ru>
2025-06-09 20:48:50 +0300

    Update quick_insert.py

--- module_10_db1/homework/hw5/quick_insert.py
+++ module_10_db1/homework/hw5/quick_insert.py
@@ -1,19 +1,23 @@
-from typing import Union, List
+def find_insert_position(arr, x):
+    """Находит позицию для вставки x в отсортированный массив arr"""
+    left, right = 0, len(arr)
+    while left < right:
+        mid = (left + right) // 2
+        if arr[mid] < x:
+            left = mid + 1
+        else:
+            right = mid
+    return left
 
-Number = Union[int, float, complex]
+# Тесты
+A = [1, 2, 3, 3, 3, 5]
+x = 4
+assert find_insert_position(A, x) == 5
+A.insert(5, x)
+assert A == sorted(A)
 
-
-def find_insert_position(array: List[Number], number: Number) -> int:
-    ...
-
-
-if __name__ == '__main__':
-    A: List[Number] = [1, 2, 3, 3, 3, 5]
-    x: Number = 4
-    insert_position: int = find_insert_position(A, x)
-    assert insert_position == 5
-
-    A: List[Number] = [1, 2, 3, 3, 3, 5]
-    x: Number = 4
-    A.insert(insert_position, x)
-    assert A == sorted(A)
+# Крайние случаи
+assert find_insert_position([], 1) == 0  # Пустой массив
+assert find_insert_position([2, 3, 5], 1) == 0  # Вставка в начало
+assert find_insert_position([2, 3, 5], 6) == 3  # Вставка в конец
+assert find_insert_position([1, 2, 2, 2, 3], 2) == 1  # Вставка среди дубликатов
