6d5f5db44dcdef9a9de32db298782fc0d4c3190f
Герман Селенских <selenskih.german@yandex.ru>
2025-06-16 14:37:15 +0300

    Update main.py

--- module_11_multitasking/homework/hw3/main.py
+++ module_11_multitasking/homework/hw3/main.py
@@ -2,25 +2,24 @@ import logging
 import random
 import threading
 import time
-from typing import List
 
-TOTAL_TICKETS: int = 10
+TOTAL_TICKETS = 10
+MAX_TICKETS = 100
+PRINT_THRESHOLD = 4
 
 logging.basicConfig(level=logging.INFO)
-logger: logging.Logger = logging.getLogger(__name__)
-
+logger = logging.getLogger(__name__)
 
 class Seller(threading.Thread):
-
-    def __init__(self, semaphore: threading.Semaphore) -> None:
+    def __init__(self, semaphore: threading.Semaphore):
         super().__init__()
-        self.sem: threading.Semaphore = semaphore
-        self.tickets_sold: int = 0
+        self.sem = semaphore
+        self.tickets_sold = 0
         logger.info('Seller started work')
 
-    def run(self) -> None:
+    def run(self):
         global TOTAL_TICKETS
-        is_running: bool = True
+        is_running = True
         while is_running:
             self.random_sleep()
             with self.sem:
@@ -28,24 +27,46 @@ class Seller(threading.Thread):
                     break
                 self.tickets_sold += 1
                 TOTAL_TICKETS -= 1
-                logger.info(f'{self.name} sold one;  {TOTAL_TICKETS} left')
-        logger.info(f'Seller {self.name} sold {self.tickets_sold} tickets')
+                logger.info(f'{self.getName()} sold one; {TOTAL_TICKETS} left')
+        logger.info(f'Seller {self.getName()} sold {self.tickets_sold} tickets')
 
-    def random_sleep(self) -> None:
+    def random_sleep(self):
         time.sleep(random.randint(0, 1))
 
+class Director(threading.Thread):
+    def __init__(self, semaphore: threading.Semaphore):
+        super().__init__()
+        self.sem = semaphore
+        logger.info('Director started work')
 
-def main() -> None:
-    semaphore: threading.Semaphore = threading.Semaphore()
-    sellers: List[Seller] = []
-    for _ in range(4):
+    def run(self):
+        global TOTAL_TICKETS
+        while True:
+            with self.sem:
+                if TOTAL_TICKETS <= PRINT_THRESHOLD and TOTAL_TICKETS + 6 <= MAX_TICKETS:
+                    added = min(6, MAX_TICKETS - TOTAL_TICKETS)
+                    TOTAL_TICKETS += added
+                    logger.info(f'Director added {added} tickets; Total now {TOTAL_TICKETS}')
+                elif TOTAL_TICKETS <= 0:
+                    break
+            time.sleep(1)
+
+def main():
+    semaphore = threading.Semaphore()
+    sellers = []
+    
+    for _ in range(3):
         seller = Seller(semaphore)
         seller.start()
         sellers.append(seller)
-
+    
+    director = Director(semaphore)
+    director.start()
+    
     for seller in sellers:
         seller.join()
-
+    
+    director.join()
 
 if __name__ == '__main__':
     main()
