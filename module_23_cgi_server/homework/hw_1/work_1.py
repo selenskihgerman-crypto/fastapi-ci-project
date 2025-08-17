6c33cab80b8e5ee0442fa7517bd9aef3f6e0ec32
Герман Селенских <selenskih.german@yandex.ru>
2025-08-17 12:08:44 +0300

    Update work_1.py

--- module_23_cgi_server/homework/hw_1/work_1.py
+++ module_23_cgi_server/homework/hw_1/work_1.py
@@ -1,16 +1,22 @@
-# 1. wsgi_app.py (Задание 1 - WSGI-приложение)
-
 import json
 import time
-from werkzeug.wrappers import Request, Response
 
 class WSGIApp:
     def __init__(self):
-        self.routes = {
-            '/hello': self.handle_hello,
-            '/hello/<name>': self.handle_hello_name,
-            '/long_task': self.handle_long_task
-        }
+        self.routes = {}
+        
+        @self.route("/hello")
+        def hello():
+            return json.dumps({"response": "Hello, world!"})
+            
+        @self.route("/hello/<name>")
+        def hello_name(name):
+            return json.dumps({"response": f"Hello, {name}!"})
+            
+        @self.route("/long_task")
+        def long_task():
+            time.sleep(300)
+            return json.dumps({"message": "We did it!"})
 
     def route(self, path):
         def decorator(func):
@@ -18,35 +24,21 @@ class WSGIApp:
             return func
         return decorator
 
-    def handle_hello(self):
-        return json.dumps({"response": "Hello, world!"})
-
-    def handle_hello_name(self, name):
-        return json.dumps({"response": f"Hello, {name}!"})
-
-    def handle_long_task(self):
-        time.sleep(300)  # 5-минутная задержка
-        return json.dumps({"message": "We did it!"})
-
     def __call__(self, environ, start_response):
-        request = Request(environ)
-        path = request.path
-
-        # Динамические маршруты
-        if path.startswith('/hello/'):
+        path = environ.get('PATH_INFO', '')
+        
+        if path == '/hello':
+            response = self.routes['/hello']()
+        elif path.startswith('/hello/'):
             name = path.split('/')[-1]
-            response = self.handle_hello_name(name)
-            start_response('200 OK', [('Content-Type', 'application/json')])
-            return [response.encode()]
-
-        # Статические маршруты
-        if path in self.routes:
-            response = self.routes[path]()
-            start_response('200 OK', [('Content-Type', 'application/json')])
-            return [response.encode()]
-
-        # 404
-        start_response('404 Not Found', [('Content-Type', 'application/json')])
-        return [json.dumps({"error": "Not Found"}).encode()]
+            response = self.routes['/hello/<name>'](name)
+        elif path == '/long_task':
+            response = self.routes['/long_task']()
+        else:
+            start_response('404 Not Found', [('Content-Type', 'application/json')])
+            return [json.dumps({"error": "Not Found"}).encode()]
+            
+        start_response('200 OK', [('Content-Type', 'application/json')])
+        return [response.encode()]
 
 app = WSGIApp()
